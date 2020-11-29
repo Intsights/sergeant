9ae3cd38575155422d013d700c83c20ea849aec4
Gal Ben David <gal@intsights.com>
2020-11-29 18:29:55 +0200

    bump to version 0.17.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='sergeant',
-    version='0.16.1',
+    version='0.17.0',
     author='Gal Ben David',
     author_email='gal@intsights.com',
     url='https://github.com/Intsights/sergeant',
