b3b3bd389aa60c0afe995dcc47e029785a8558fd
Gal Ben David <gal@intsights.com>
2021-01-26 13:58:16 +0200

    wip

--- tests/logging/test_logstash.py
+++ tests/logging/test_logstash.py
@@ -41,7 +41,7 @@ class LoggingLogstashTestCase(
             msg='test',
         )
 
-        message = orjson_mock.dumps.call_args.args[0]
+        message = orjson_mock.dumps.call_args[0][0]
 
         self.assertEqual(
             first=message['emitter']['pathname'],
